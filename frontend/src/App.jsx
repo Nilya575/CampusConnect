a736e2e83fcf160c1b98b7c3cc468e408ada3dfe
nilya mishra <nilyamishra05@gmail.com>
2026-09-24 20:07:07 +0530

    Update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 import Signup from './pages/Signup';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Notices from './pages/Notices';
 import Events from './pages/Events';
